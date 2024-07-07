390aa0b50987156d80ef1c9a2f2bc1e968f0b276
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-07-07 22:24:18 +0300

    fix: update eslint

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -6,22 +6,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react-compiler/recommended',
     'prettier',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react-compiler', 'react', '@typescript-eslint'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'react-compiler'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react-compiler/no-unsupported-browser-features': 'error',
     'react/react-in-jsx-scope': 'off',
     'prettier/prettier': 'error',
+    "react-compiler/react-compiler": "error",
   },
   settings: {
     react: {
