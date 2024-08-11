9fc0188b06dfbfc4a55a23a81674bf5034dd63c9
Serge Fedorov <Lir2ooo5@yandex.ru>
2024-08-11 23:10:08 +0300

    feat: migrate to next

---
+++ class-components/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Star Wars',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body>
+        <div id='root'>{children}</div>
+        <script type='module' src='/src/main.tsx'></script>
+      </body>
+    </html>
+  );
+}
